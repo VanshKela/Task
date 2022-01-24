e36c3809bf1871d215df8810ed9a86d3846e0e8e
VanshKela <vanshkela2@gmail.com>
2022-01-25 02:21:55 +0530

    Workspace added

--- task-ringover/components/Workspace/workspace.js
+++ task-ringover/components/Workspace/workspace.js
@@ -1,7 +1,26 @@
-import React from 'react';
-import styles from '../../styles/Home.module.scss'
+import React from "react";
+import styles from "../../styles/Home.module.scss";
+import record from "../../images/record_voice_over.png";
+import note from "../../images/note.png";
+import Image from "next/image";
+import noWork from "../../images/noWork.png";
 function Workspace() {
-  return <div className={styles.workspace}><h1>Workspace</h1></div>;
+  return (
+    <div className={styles.workspace}>
+      <div className={styles.heading}>
+          <h1 style={{textAlign:"left"}}>Workspace</h1>
+          <div className={styles.actions}>
+              <Image src={record} alt="record" />
+              <Image src={note} alt="note" />
+          </div>
+      </div>
+      <div className={styles.main}>
+        <Image src={noWork} alt="No-Work" />
+        <h3>Your workspace is empty!</h3>
+        <p>To add a tab, click on any option on your bottom left</p>
+      </div>
+    </div>
+  );
 }
 
 export default Workspace;
