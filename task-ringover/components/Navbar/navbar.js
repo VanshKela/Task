eccef27e221bb1d364dc669fe8da8bcd6d171cf3
VanshKela <vanshkela2@gmail.com>
2022-01-24 21:50:23 +0530

    Navbar done

--- task-ringover/components/Navbar/navbar.js
+++ task-ringover/components/Navbar/navbar.js
@@ -1,11 +1,28 @@
-import React from 'react'
-import styles from '../../styles/Home.module.scss'
+import React from "react";
+import styles from "../../styles/Home.module.scss";
+import logo from "../../images/logo.png";
+import vector from "../../images/Vector.jpg";
+import vector1 from "../../images/Vector-1.jpg";
+import photo from "../../images/photo.jpg";
+import Image from "next/image";
+
 function Navbar() {
-    return (
-        <div className={styles.navbar}>
-            <h1>Navbar</h1>
-        </div>
-    )
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.logo}>
+        <Image className={styles.img} src={logo} alt="logo" />
+        <h3 className={styles.text}>Sales Machine</h3>
+      </div>
+      <div className={styles.searchbar}>
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className={styles.pannel}>
+        <Image className={styles.profileImage} src={photo} alt="logo" />
+        <Image className={styles.img} src={vector1} alt="logo" />
+        <Image className={styles.img} src={vector} alt="logo" />
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- task-ringover/components/Profile/profile.js
+++ task-ringover/components/Profile/profile.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import styles from '../../styles/Home.module.scss'
+import React from "react";
+import styles from "../../styles/Home.module.scss";
 function Profile() {
-  return <div className={styles.profile}><h1>Profile</h1></div>;
+  return (
+    <div className={styles.profile}>
+      <div className={styles.credentials}>
+          <h3 className={styles.name}>Jean-Samuel Najnudel</h3>
+      </div>
+      <div className={styles.topics}>
+          <h3>Topics</h3>
+      </div>
+      <div className={styles.contact}>
+          <h3>Contacts</h3>
+      </div>
+      <div className={styles.additional}>
+          <h3>Additional</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
