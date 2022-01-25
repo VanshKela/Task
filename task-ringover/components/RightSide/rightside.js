9799d982264d62c3d7df282c90bdba47dd5d4ec1
VanshKela <vanshkela2@gmail.com>
2022-01-25 10:22:54 +0530

    Rght side started

--- task-ringover/components/RightSide/rightside.js
+++ task-ringover/components/RightSide/rightside.js
@@ -3,7 +3,8 @@ import styles from '../../styles/Home.module.scss'
 function RightSide() {
     return (
         <div className={styles.rightSide}>
-            <h1>RightSide</h1>
+            <div className={styles.actions}>Top</div>
+            <div className={styles.pannel}>Bottom</div>
         </div>
     )
 }
